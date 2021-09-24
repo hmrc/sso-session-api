2a2d618bf4235b8abc1268f8680caf94eb4fcc7e
Omer Arshad <omer.arshad@digital.hmrc.gov.uk>
2021-09-24 04:41:32 +0100

    GG-5817 : Play 2.8 Upgrade - sso-* services

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,12 @@ private object AppDependencies {
     ehcache,
     // claims to be a frontend even though it's definitely a backend so it has access to `sso.encryption.key`,
     // and because it appears to need to set cookies
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.7.0",
-    "uk.gov.hmrc" %% "government-gateway-domain" % "6.4.0-play-27",
-    "uk.gov.hmrc" %% "govuk-template" % "5.69.0-play-27"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.14.0",
+    "uk.gov.hmrc" %% "government-gateway-domain" % "7.0.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "government-gateway-test" % "4.4.0-play-27" % "test,it"
+    "uk.gov.hmrc" %% "government-gateway-test" % "4.6.0-play-28" % "test,it"
   )
 
   def apply() = compile ++ test
