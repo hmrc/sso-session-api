4ae04764a78b8534a5f5d9dee63f6297ba3c301d
ShaneTN <58428357+ShaneTN@users.noreply.github.com>
2022-06-22 12:23:57 +0100

    GG-6442 updated logger + lib versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,12 +8,12 @@ private object AppDependencies {
     ehcache,
     // claims to be a frontend even though it's definitely a backend so it has access to `sso.encryption.key`,
     // and because it appears to need to set cookies
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc" %% "government-gateway-domain" % "7.0.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc" %% "government-gateway-domain" % "7.1.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "government-gateway-test" % "4.6.0-play-28" % "test,it"
+    "uk.gov.hmrc" %% "government-gateway-test" % "4.9.0" % "test,it"
   )
 
   def apply() = compile ++ test
