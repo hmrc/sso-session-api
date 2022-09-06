0039e176ce8aa941ef111518d45e690f88ab991c
Inbar Vernia <104011282+inbar-vernia@users.noreply.github.com>
2022-09-06 12:28:37 +0100

    GG-6456: Upgraded Bootstrap/Play + other lib dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,8 @@ private object AppDependencies {
     ehcache,
     // claims to be a frontend even though it's definitely a backend so it has access to `sso.encryption.key`,
     // and because it appears to need to set cookies
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc" %% "government-gateway-domain" % "7.1.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.2.0",
+    "uk.gov.hmrc" %% "government-gateway-domain" % "7.2.0-play-28"
   )
 
   val test = Seq(
