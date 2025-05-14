4ca6078780c7030439e9f74f65efe4e3992f3c30
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2025-05-14 12:20:49 +0100

    [OLFG-2399] Added CA resource client

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,8 @@ private object AppDependencies {
     caffeine,
     // claims to be a frontend even though it's definitely a backend so it has access to `sso.encryption.key`,
     // and because it appears to need to set cookies
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc" %% "centralised-authorisation-resource-client-play-30" % "1.5.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
